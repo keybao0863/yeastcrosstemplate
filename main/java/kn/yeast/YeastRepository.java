5a3ad0e18c75188d39b75e8ec34d0397dfa2c7f2
unknown <keybao0863@gmail.com>
2019-03-25 22:47:38 -0400

    assssasa

--- main/java/kn/yeast/YeastRepository.java
+++
@@ -1,8 +0,0 @@
-package kn.yeast;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface YeastRepository extends CrudRepository<Master, String> {
-	public Master findByName(String name);
-
-}
