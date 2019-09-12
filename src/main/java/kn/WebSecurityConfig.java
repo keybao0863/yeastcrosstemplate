ca31f71f87f9613eff6bf3710f63b6befbc037eb
keybao0863 <36831626+keybao0863@users.noreply.github.com>
2019-09-12 11:37:01 -0400

    Update WebSecurityConfig.java

--- src/main/java/kn/WebSecurityConfig.java
+++ src/main/java/kn/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home", "/addstrain", "/crosstemplate").permitAll()
+                .antMatchers("/", "/home", "/addstrain", "/crosstemplate", "/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
