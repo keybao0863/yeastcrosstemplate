e23a7c5dc527cef02e85a9759e517e77de58d4e1
Kehan <keybao0863@gmail.com>
2019-10-01 22:21:12 -0400

    styling

--- src/main/java/kn/WebSecurityConfig.java
+++ src/main/java/kn/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package kn;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home", "/addstrain", "/crosstemplate", "appstyle.css").permitAll()
+            	.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/", "/home", "/addstrain", "/crosstemplate", "/resources/static/appstyle.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
